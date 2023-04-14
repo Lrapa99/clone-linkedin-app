bac83a13781c1ab78aed4a465f4ae84d60cdf3d9
lrapa99 <luisrapalino88@gmail.com>
2023-04-14 14:57:25 -0500

    add the component for feeds

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { PostsComponent } from './components/posts/posts.component';
+import { FeedComponent } from './components/feed/feed.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// angular material
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+    PostsComponent,
+    FeedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
